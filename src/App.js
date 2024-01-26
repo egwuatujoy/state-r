6c03e3f31790d6ccfa8b32b1351be273fac4d64e
joyegwuatu <egwuatujoy60@gmail.com>
2024-01-26 15:29:47 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      person: {
+        fullName: "Joy Egwuatu",
+        bio: "I am a front-end developer......",
+        imgSrc: "https://images.pexels.com/photos/1130626/pexels-photo-1130626.jpeg?auto=compress&cs=tinysrgb&w=600",
+        profession: "Software Engineer"
+      },
+      shows: false,
+      elapsedTime: 0
+    };
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      elapsedTime: this.state.elapsedTime + 1
+    });
+  }
+
+  toggleProfile = () => {
+    this.setState({ shows: !this.state.shows });
+  }
+
+  render() {
+    const { person, shows, elapsedTime } = this.state;
+
+    return (
+      <div className="App">
+        <button onClick={this.toggleProfile}>Toggle Profile</button>
+        {shows && (
+          <div>
+            <h2>{person.fullName}</h2>
+            <img src={person.imgSrc} alt="Profile" style={{width: "200px"}} />
+            <p>Bio: {person.bio}</p>
+            <p>Profession: {person.profession}</p>
+          </div>
+        )}
+        <p>Time since last component mount: {elapsedTime} seconds</p>
+      </div>
+    );
+  }
 }
 
 export default App;
